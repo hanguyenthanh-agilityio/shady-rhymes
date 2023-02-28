50a891e7df30c501f071d28c96c37f3d0067e09f
“ha.nguyenthanh” <ha.nguyenthanh@asnet.com.vn>
2023-02-28 09:51:17 +0700

    feat: update config Chakra ui

--- shady-rhymes/pages/_app.tsx
+++ shady-rhymes/pages/_app.tsx
@@ -3,23 +3,12 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
 // 1. Import the extendTheme function
-import { extendTheme } from '@chakra-ui/react';
-
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac'
-  }
-};
-
-const theme = extendTheme({ colors });
+import CHAKRA_THEME_DEFAULT from '../theme/chakra';
 
 // 3. Pass the `theme` prop to the `ChakraProvider`
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={CHAKRA_THEME_DEFAULT}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
