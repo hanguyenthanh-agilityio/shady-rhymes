e9f9cf496c78c0e94b4a471172f89bd38d8f7692
“ha.nguyenthanh” <ha.nguyenthanh@asnet.com.vn>
2023-03-01 17:27:35 +0700

    feat: implement card and rating component

---
+++ shady-rhymes/theme/components/heading.ts
@@ -0,0 +1,15 @@
+export const Heading = {
+  baseStyle: {
+    color: 'heading.default',
+    fontWeight: '700'
+  },
+  sizes: {
+    small: {
+      fontSize: '18px',
+      lineHeight: '36px'
+    }
+  },
+  defaultProps: {
+    size: 'small'
+  }
+};
---
+++ shady-rhymes/theme/components/text.ts
@@ -0,0 +1,23 @@
+export const Text = {
+  baseStyle: {
+    color: 'text.default',
+    fontWeight: '400'
+  },
+
+  sizes: {
+    small: {
+      fontSize: '14px',
+      lightHeight: '28px'
+    }
+  },
+
+  variants: {
+    helper: {
+      color: 'text.helper'
+    }
+  },
+
+  defaultProps: {
+    size: 'small'
+  }
+};
