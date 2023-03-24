5b5ed77ffddbc6c260357649ac09e686adc3a6b5
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-03-24 11:49:24 +0700

    feat: config google analytics

--- shady-rhymes/components/SEO/index.tsx
+++ shady-rhymes/components/SEO/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 
 interface SEOProps {
   title: string;
@@ -7,16 +8,33 @@ interface SEOProps {
 
 const SEO = ({ title, description }: SEOProps) => {
   return (
-    <Head>
-      {/* Title Name */}
-      <title>{title}</title>
+    <>
+      <Head>
+        {/* Title Name */}
+        <title>{title}</title>
 
-      {/* Adding additional information about website */}
-      <meta name="description" content={description} />
+        {/* Adding additional information about website */}
+        <meta name="description" content={description} />
 
-      {/* Adding custom favicon */}
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+        {/* Adding custom favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-K7MFK4V1DK"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+        
+            gtag('config', 'G-K7MFK4V1DK');
+        `}
+      </Script>
+    </>
   );
 };
 
